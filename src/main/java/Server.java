4c3906fcbdd4d0d9d762e56875a5346d644eb4ea
vivanpoddar <vivanneil@outlook.com>
2020-08-23 00:17:02 -0400

    Initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,49 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    public Server(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("server started");
+
+            System.out.println("waiting for client");
+
+            socket = server.accept();
+            System.out.println("client accepted");
+
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            while (!line.equals("i done")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("closing connection");
+
+            socket.close();
+            in.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Server server = new Server(5000);
+    }
+}
+

8dcb44f6e7e84cfacc38ee8aca944c8525ccd839
BurgBoi <davidwangshark@gmail.com>
2020-08-23 04:58:19 -0400

    added code to save data to a json file in Users/ directory

--- src/main/java/me/quicksourceserver/Register.java
+++ src/main/java/me/quicksourceserver/Register.java
@@ -1,6 +1,10 @@
 package me.quicksourceserver;
 
+import org.json.simple.JSONObject;
+
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,12 +26,30 @@ public class Register {
 
 	}
 
-	public void makeLogin(String username, String password) {
+	public void makeLogin(String username, String password, AccountType accountType) {
 		try {
-			Fil
+			File file = new File("Users/", username);
+			File dir = new File("Users/");
 
-		} catch (IOException e) {
-			System.out.println("");
+			if (!dir.exists())
+				dir.mkdir();
+			if (!file.exists())
+				file.createNewFile();
+			else {
+				//return "that username already exists!" to the client
+			}
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("username", username);
+			jsonObject.put("password", password);
+			jsonObject.put("accountType", accountType.getRankType().getKey());
+
+			try (FileWriter fileWriter = new FileWriter(file)) {
+				fileWriter.write(jsonObject.toString());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -37,10 +59,10 @@ public class Register {
 
 		} finally {
 			if (input != null) {
-                input.close();
+				input.close();
 			}
 			if (output != null) {
-                output.close();
+				output.close();
 
 			}
 		}

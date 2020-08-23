7a88efeff43723203eba160ff06035c1b69249e6
BurgBoi <davidwangshark@gmail.com>
2020-08-23 05:18:24 -0400

    added code to checkPassword in the class Login

--- src/main/java/me/quicksourceserver/LogIn.java
+++ src/main/java/me/quicksourceserver/LogIn.java
@@ -1,22 +1,35 @@
 package me.quicksourceserver;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
+
 public class LogIn {
 
-	java.io.FileReader input = null;
-	java.io.FileWriter output = null;
-
-	public void checkPassword() throws java.io.IOException {
-		try {
-
-		} finally {
-			if (input != null) {
-                input.close();
+	public boolean checkPassword(String username, String password) throws java.io.IOException {
+		File file = new File("Users/", username);
+		if (file.exists()) {
+			try {
+				try (FileReader fileReader = new FileReader(file)) {
+					JSONParser jsonParser = new JSONParser();
+					JSONObject jsonObject = (JSONObject) jsonParser.parse(fileReader);
+					String passwordFromJson = (String) jsonObject.get("password");
+					if (password.equals(passwordFromJson)) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			if (output != null) {
-                output.close();
-
-			}
+		} else {
+			//return to the client "that user does not exist!"
+			return false;
 		}
+		return false;
 	}
 }
-}
+
--- src/main/java/me/quicksourceserver/Register.java
+++ src/main/java/me/quicksourceserver/Register.java
@@ -2,31 +2,17 @@ package me.quicksourceserver;
 
 import org.json.simple.JSONObject;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Register {
-
-	private Socket socket = null;
-	private ServerSocket server = null;
-	private DataInputStream in = null;
-
-	java.io.FileReader input = null;
-	java.io.FileWriter output = null;
-
-	int check;
-
-//			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
 	public Register() throws IOException {
 
 	}
 
-	public void makeLogin(String username, String password, AccountType accountType) {
+	public void createUserFile(String username, String password, AccountType accountType) {
 		try {
 			File file = new File("Users/", username);
 			File dir = new File("Users/");
@@ -50,21 +36,6 @@ public class Register {
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-	}
-
-
-	public void checkPassword() throws java.io.IOException {
-		try {
-
-		} finally {
-			if (input != null) {
-				input.close();
-			}
-			if (output != null) {
-				output.close();
-
-			}
 		}
 	}
 }

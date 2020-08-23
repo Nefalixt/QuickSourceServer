07e81b7762813b1cbab409c574c9ceb604ea2f31
vivanpoddar <vivanneil@outlook.com>
2020-08-23 05:20:28 -0400

    changed txt to JSON

--- src/main/java/me/quicksourceserver/LogIn.java
+++ src/main/java/me/quicksourceserver/LogIn.java
@@ -2,21 +2,11 @@ package me.quicksourceserver;
 
 public class LogIn {
 
-	java.io.FileReader input = null;
-	java.io.FileWriter output = null;
+    java.io.FileReader input = null;
+    java.io.FileWriter output = null;
 
-	public void checkPassword() throws java.io.IOException {
-		try {
-
-		} finally {
-			if (input != null) {
-                input.close();
-			}
-			if (output != null) {
-                output.close();
-
-			}
-		}
-	}
+    public void checkLogin(String username, String password) {
+        if ()
+    }
 }
-}
+
--- src/main/java/me/quicksourceserver/Main.java
+++ src/main/java/me/quicksourceserver/Main.java
@@ -1,14 +1,14 @@
 package me.quicksourceserver;
 
 class Main {
-	public static void main(String args[]) {
-		ServerUtils server = new ServerUtils(5000);
-		try {
-			Register test = new Register();
-			test.copyFile();
-		} catch (java.io.IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String args[]) {
+        ServerUtils server = new ServerUtils(5000);
+        try {
+            AccountCreate test = new AccountCreate();
+            test.copyFile();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
